a0bb9e80441f3ded013e5a577d7f6cbe316c2d34
Serhii <serhii.soldatov@gmail.com>
2022-05-06 12:31:48 +0200

    Task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,3 +2,15 @@
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // -1, -7, 567, 89, 223-> 3
+
+System.Console.WriteLine("Введите любые натуральные числа через пробел:");
+string a = Console.ReadLine();
+string[] arr = a.Split(" ");
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    int some = int.Parse(arr[i]);
+    if (some > 0) count++;
+}
+System.Console.WriteLine($"Кол-во чисел, значение которых больше 0: {count}");
